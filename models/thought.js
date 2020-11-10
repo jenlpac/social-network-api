0149e2891ccf17c225903ad7f33d998609cc4c46
Jenniffer <jenlpac@yahoo.com>
2020-11-10 09:30:48 -0600

    include virtual and export Thought

--- models/thought.js
+++ models/thought.js
@@ -63,3 +63,11 @@ const ThoughtSchema = new Schema({
     id: false
 });
 
+// Virtual to retrieve the length of the reactions array:
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought, ThoughtSchema');
+
+module.exports = Thought;
