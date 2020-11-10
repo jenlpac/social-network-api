fcc4122feab93569abc91134dd5cc9423da202a8
Jenniffer <jenlpac@yahoo.com>
2020-11-10 08:49:31 -0600

    create user schema

--- models/user.js
+++ models/user.js
@@ -1,2 +1,39 @@
 const { Schema, model } = require('mongoose');
 
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,3})$/
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+    
+}
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
